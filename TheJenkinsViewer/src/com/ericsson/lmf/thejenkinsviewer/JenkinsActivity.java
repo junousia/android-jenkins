cf93441c2b91d229b5acb361c03177406748d1fe
Vidhuran Harichandra Babu <vidhuran.harichandra.babu@ericsson.com>
2013-05-15 14:12:09 +0300

    Added ListView

--- TheJenkinsViewer/src/com/ericsson/lmf/thejenkinsviewer/JenkinsActivity.java
+++ TheJenkinsViewer/src/com/ericsson/lmf/thejenkinsviewer/JenkinsActivity.java
@@ -7,15 +7,22 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class JenkinsActivity extends Activity {
 	private static final String TAG = JenkinsActivity.class.getSimpleName();
-
+	private TextView textView;
+	private ListView listView;
+	
 	@Override
 	public void onCreate(Bundle args) {
 		Log.d(TAG, "onCreate called");
 		super.onCreate(args);
 		setContentView(R.layout.main_layout);
+		textView = (TextView)findViewById(R.id.jenkinsInfo);
+		listView = (ListView)findViewById(R.id.jenkinsList);
+		listView.setVisibility(View.INVISIBLE);
 		Button button = (Button) findViewById(R.id.jenkinsInfoButton);
 		button.setOnClickListener(refreshListener);
 	}
@@ -27,6 +34,8 @@ public class JenkinsActivity extends Activity {
 			Log.d(TAG, "Refresh button clicked, starting service");
 			Intent intent = new Intent(JenkinsActivity.this, JenkinsIntentService.class);
 			startService(intent);
+			textView.setVisibility(View.GONE);
+			listView.setVisibility(View.VISIBLE);
 		}
 	};
 }

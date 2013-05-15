b2c91d3b21b236f9e3b8d84d311d54cfd448aafe
Thomas Bergenwall <thomas.bergenwall@ericsson.com>
2013-05-15 11:16:11 +0300

    Added some new stuff..

--- TheJenkinsViewer/src/com/ericsson/lmf/thejenkinsviewer/JenkinsActivity.java
+++ TheJenkinsViewer/src/com/ericsson/lmf/thejenkinsviewer/JenkinsActivity.java
@@ -1,7 +1,14 @@
 package com.ericsson.lmf.thejenkinsviewer;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class JenkinsActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle args) {
+		super.onCreate(args);
+		setContentView(R.layout.main_layout);
+	}
 
 }
